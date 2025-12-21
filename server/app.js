4bd365d746fefb0cad5298c18c60b210a051311c
Roman Zamyatin <Romikz12@mail.ru>
2025-12-21 23:13:51 +0300

    connected the client to the server

--- server/app.js
+++ server/app.js
@@ -18,7 +18,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', // React-клиент
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
